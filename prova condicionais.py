81a63c955de14813bd1f1e6f81fd9ad2345233e2
DanielPolicarpo85 <153463353+DanielPolicarpo85@users.noreply.github.com>
2026-01-12 20:46:32 -0300

    Add files via upload

---
+++ prova condicionais.py
@@ -0,0 +1,9 @@
+numero=int(input("digite um numero: "))
+
+
+if numero > 0:
+   print("numero é positivo")
+elif numero < 0:
+    print("numero é negativo")
+else:
+    print("o numero digitado é zero")    
